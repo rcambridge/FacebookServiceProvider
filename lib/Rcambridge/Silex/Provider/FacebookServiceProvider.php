a9ea39ab31983174fc7c14df7abb899d724b5f65
Robert Cambridge <robert@boxlightmedia.com>
2013-02-13 12:18:35 +0000

    use overriding class which can be customised

--- lib/Rcambridge/Silex/Provider/FacebookServiceProvider.php
+++ lib/Rcambridge/Silex/Provider/FacebookServiceProvider.php
@@ -9,12 +9,14 @@ namespace Rcambridge\Silex\Provider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+use Rcambridge\Silex\Api\Facebook;
+
 class FacebookServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
         $app['facebook'] = $app->share(function () use ($app) {
-            return new \Facebook(array(
+            return new Facebook(array(
                 'appId'  => $app['facebook.app_id'],
                 'secret' => $app['facebook.secret'],
             ));
